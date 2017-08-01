51bf3e46826ede00480eb0bd536ce50835d3f801
Tang Chuan-Wei <xyzith@users.noreply.github.com>
2017-08-01 17:30:59 +0800

    refractoring, add feature group by pk

--- group.user.js
+++ group.user.js
@@ -4,7 +4,7 @@
 // @updateURL       https://raw.githubusercontent.com/xyzith/wod_item_group/master/group.user.js
 // @grant           none
 // @author          Taylor Tang
-// @version         1.9
+// @version         2.0 
 // @description     Add item group feature
 // @include         *://*.world-of-dungeons.org/wod/spiel/hero/items.php*
 // ==/UserScript==
@@ -13,13 +13,16 @@
     var LANGUAGE = Object.freeze({
         COIN: '金币',
         ITEM: '物品',
-        GROUP: '团队',
+        GROUP_ITEM: '团队物品',
         SELL: '出售',
         POSITION: '位置',
         WAREHOUSE: '仓库',
         GROUP_WAREHOUSE2: '团队仓库',
         GROUP_WAREHOUSE: '宝库',
-        STORAGE_ROOM: '贮藏室'
+        STORAGE_ROOM: '贮藏室',
+        OWNER: '所有者',
+        STORAGE_DATE: '入库时间',
+        DROP_DATE: '掉落',
     });
 
     function addCss() {
@@ -31,8 +34,8 @@
             var row0_color = getComputedStyle(row0).getPropertyValue('background-color');
             var row1_color = getComputedStyle(row1).getPropertyValue('background-color');
             style.sheet.insertRule('table.content_table .hidden_row { display: none; }', 0);
-            style.sheet.insertRule('table.content_table > tbody > :nth-child(2n) { background-color: ' + row0_color + '; }', 0);
-            style.sheet.insertRule('table.content_table > tbody > :nth-child(2n+1) { background-color: ' + row1_color + '; }', 0);
+            style.sheet.insertRule('table.content_table > tbody > :nth-child(2n) { background-color: ' + row1_color + '; }', 0);
+            style.sheet.insertRule('table.content_table > tbody > :nth-child(2n+1) { background-color: ' + row0_color + '; }', 0);
         }
     }
 
@@ -45,7 +48,8 @@
     }
 
     function chomp(str) {
-        return str.replace(/[ \xA0\n\t\r]*/g, '');
+        var regex = new RegExp('[ ' + String.fromCharCode(8593) + String.fromCharCode(8595) + '\xA0\t\n\r]|&\w+;', 'g');
+        return str.replace(regex, '');
     }
 
     function findTable() {
@@ -63,53 +67,85 @@
         return findSiblings(search);
     }
 
-    function Item(row) {
+    function Item(row, table_map) {
+        var head = row.parentNode.parentNode.tHead.querySelector('.header');
         var name = row.cells[1];
         var use = name.textContent.match(/\((\d+)\/\d+\)$/);
-        var price_el = this.getPrice(row);
 
         this.el = row;
-        this.name = name.querySelector('a').textContent.replace(/!$/, '');
-        this.item_useability = name.querySelector('a').className;
-        this.use = use ? Number(use[1]) : 1;
-        this.price = price_el ? Number(price_el.textContent) : 0;
-        this.sell_checkbox = price_el ? price_el.querySelector('input') : null;
-        this.group_item_checkbox = this.getGroupItemCheckbox(row);
-        this.item_position_select = this.getItemPositionSelect(row);
+        for(var i = 0; i < table_map.length; i++) {
+            if( table_map[i] in this.parser) {
+                this.parser[table_map[i]].call(this, i);
+            }
+        }
     }
 
-    Item.prototype.getPrice = function(el) {
-        var price = el.querySelector('img[title="' + LANGUAGE.COIN + '"]');
-        if(price) {
-            return price.parentNode;
+    Item.prototype.parser = Object.freeze({
+        sell: function(idx) {
+            var price = this.el.cells[idx];
+            if(!price) { return false; }
+            this.price = Number(price.textContent || 0);
+            this.sell_checkbox = price.querySelector('input');
+        },
+        item: function(idx) {
+            var item = this.el.cells[idx];
+            var use = item.textContent.match(/\((\d+)\/\d+\)$/);
+            item = item.querySelector('a');
+
+            this.item = item.textContent.replace(/!$/, '');
+            this.use = use ? Number(use[1]) : 1;
+            this.item_useability = item.className;
+        },
+        owner: function(idx) {
+            var owner = this.el.cells[idx];
+            this.owner = owner.textContent;
+        },
+        group_item: function(idx) {
+            var el = this.el.cells[idx];
+            this.group_item_checkbox = el.querySelector('input[name="SetGrpItem"]');
+        },
+        position: function(idx) {
+            var el = this.el.cells[idx];
+            this.item_position_select = el.querySelector('select');
+        },
+        storage_date: function(idx) {
+            var storage_date = this.el.cells[idx];
+            this.storage_date = storage_date.textContent;
+        },
+        drop_date: function(idx) {
+            var drop_date = this.el.cells[idx];
+            this.drop_date = drop_date.textContent;
         }
-        return null;
-    };
+    });
 
-    Item.prototype.getGroupItemCheckbox = function(el) {
-        return el.querySelector('input[name^="SetGrpItem"]');
-    };
-
-    Item.prototype.getItemPositionSelect = function(el) {
-        return el.querySelector('select[name^="EquipItem"]');
-    };
-
-    function ItemGroup() {
+    function ItemGroup(table, prime_key) {
         function useGetter() {
             var use = this.child.map((k) => (k.use));
             return use.reduce((sum, value) => (sum + value));
         }
+
         function priceGetter() {
-            var use = this.child.map((k) => (k.price));
-            return use.reduce((sum, value) => (sum + value));
+            return this.child.map((k) => (k.price)).reduce((sum, value) => (sum + value));
         }
 
-        function indexGetter() {
-            return this.child[0].el.cells[0].textContent;
+        function rowGetter() {
+            if(!row) {
+                row = this.createContainer();
+            }
+            return row;
         }
 
+        var row;
+
+        this.pk = prime_key;
+        this.tbody = table.tBodies[0];
+        this.thead = table.tHead.rows[1];
         this.child = [];
 
+        Object.defineProperty(this, 'row', {
+            get: rowGetter.bind(this),
+            writeable: false
+        });
         Object.defineProperty(this, 'use', {
             get: useGetter.bind(this),
             writeable: false
@@ -119,16 +155,10 @@
             get: priceGetter.bind(this),
             writeable: false
         });
-
-        Object.defineProperty(this, 'index', {
-            get: indexGetter.bind(this),
-            writeable: false
-        });
     }
 
     ItemGroup.prototype.add = function(item) {
         this.child.push(item);
-        this.name = item.name;
         function setupSyncEvent(key) {
             if(!item[key]) { return false; }
             if(item[key].tagName.toLowerCase() === 'select') {
@@ -146,11 +176,11 @@
         setupSyncEvent.call(this, 'item_position_select');
     };
 
-    ItemGroup.prototype.renderIndex = function(row) {
-        var index = row.insertCell();
-        index.style.textAlign = 'right';
-        index.textContent = this.index;
+    ItemGroup.prototype.renderDefault = function(cell, idx, align) {
+        cell.style.textAlign = align || 'center';
+        cell.textContent = this.child[0].el.cells[idx].textContent;
     };
+
     ItemGroup.prototype.syncSelect = function(key) {
         var el = this[key];
         var prev, now, select;
@@ -169,6 +199,7 @@
             el.value = '0';
         }
     };
+
     ItemGroup.prototype.syncCheckbox = function(key) {
         var el = this[key];
         var prev, now, check;
@@ -185,15 +216,16 @@
         }
         el.checked = now;
     };
-    ItemGroup.prototype.renderItemPosition = function(row, head_cell) {
+
+    ItemGroup.prototype.renderItemPosition = function(position, idx) {
         function newOps(txt, value) {
             var opt = document.createElement('option');
             opt.textContent = txt;
             opt.value = value;
             return opt;
         }
-        var position = row.insertCell();
         var select = document.createElement('select');
+        var head_cell = this.thead.cells[idx];
 
         position.style.textAlign = 'right';
         select.appendChild(newOps('-------', '0'));
@@ -215,12 +247,12 @@
         head_cell.querySelector('select').addEventListener('change', (function() {
             this.syncSelect('item_position_select');
         }).bind(this));
-
     };
-    ItemGroup.prototype.renderItemPrice = function(row, head_cell) {
-        var price = row.insertCell();
+
+    ItemGroup.prototype.renderItemPrice = function(price, idx) {
         var text = document.createElement('span');
         var checkbox = document.createElement('input');
+        var head_cell = this.thead.cells[idx];
         price.style.textAlign = 'right';
         text.textContent = this.price;
         checkbox.type = 'checkbox';
@@ -240,9 +272,9 @@
         }).bind(this));
     };
 
-    ItemGroup.prototype.renderGroupSetter = function(row, head_cell) {
-        var cell = row.insertCell();
+    ItemGroup.prototype.renderGroupSetter = function(cell, idx) {
         var setter = document.createElement('input');
+        var head_cell = this.thead.cells[idx];
         cell.style.textAlign = 'center';
         setter.type = 'checkbox';
         setter.addEventListener('change', (function(e){
@@ -260,17 +292,18 @@
         }).bind(this));
     };
 
-    ItemGroup.prototype.renderItemName = function(row) {
-        var text = row.insertCell();
-        var a = document.createElement('a');
-        a.innerHTML = '&#128194 ';
-        a.textContent += this.name + ' (' + this.use + ')';
-        a.className = this.child[0].item_useability;
-        text.appendChild(a);
+    ItemGroup.prototype.renderItemName = function(name) {
+        if(this.pk === 'item') {
+            var a = document.createElement('a');
+            a.innerHTML = '&#128194 ';
+            a.textContent += this.child[0].item + ' (' + this.use + ')';
+            a.className = this.child[0].item_useability;
+            name.appendChild(a);
+        }
     };
 
     ItemGroup.prototype.toggleChild = function() {
-        if(!this.row) { return false; }
+        if(this.child.length <= 1) { return false; }
         if(this.child[0].el.classList.contains('hidden_row')) {
             this.expand();
         } else {
@@ -278,8 +311,8 @@
         }
     };
 
-    ItemGroup.prototype.createContainer = function(table) {
-        var row = table.querySelector('tbody').insertRow();
+    ItemGroup.prototype.createContainer = function() {
+        var row = this.tbody.insertRow();
         row.className = 'item_group';
         row.style.cursor = 'pointer';
         row.addEventListener('click', (function(e) {
@@ -291,38 +324,49 @@
         return row;
     };
 
-    ItemGroup.prototype.parseCell = function(cell, row) {
-        var btn, title = (btn = cell.querySelector('input[type="submit"]')) ? btn.value : cell.textContent;
-        if(cell == cell.parentNode.cells[0]) {
-            this.renderIndex(row);
-        } else if(title.match(LANGUAGE.GROUP)) {
-            this.renderGroupSetter(row, cell);
-        } else if(title.match(LANGUAGE.SELL)) {
-            this.renderItemPrice(row, cell);
-        } else if(title.match(LANGUAGE.ITEM)) {
-            this.renderItemName(row);
-        } else if(title.match(LANGUAGE.POSITION)) {
-            this.renderItemPosition(row, cell);
-        } else {
-            row.insertCell();
+    ItemGroup.prototype.parseCell = function(type, idx) {
+        var cell = this.row.insertCell();
+        switch(type) {
+            case 'index':
+                this.renderDefault(cell, idx, 'right');
+                break;
+            case 'item':
+                this.renderItemName(cell);
+                break;
+            case 'sell':
+                this.renderItemPrice(cell, idx);
+                break;
+            case 'position':
+                this.renderItemPosition(cell, idx);
+                break;
+            case 'group_item':
+                this.renderGroupSetter(cell, idx);
+                break;
+            case 'owner':
+            case 'storage_date':
+            case 'drop_date':
+                if(this.pk === type) {
+                    this.renderDefault(cell, idx);
+                }
+            break;
         }
     };
 
-    ItemGroup.prototype.render = function(table, idx) {
-        var head = table.tHead.querySelector('.header');
-
-        if(this.child.length != 1) {
-            this.row = this.createContainer(table);
-            for(var i = 0; i < head.children.length; i++) {
-                this.parseCell(head.children[i], this.row);
-            }
-            this.syncSelect('item_position_select');
-            this.syncCheckbox('group_item_checkbox');
-            this.syncCheckbox('sell_checkbox');
-            this.child.reverse();
+    ItemGroup.prototype.render = function(table_map) {
+        if(this.child.length > 1) {
+            this.renderGroup(table_map);
         } else {
-            table.tBodies[0].appendChild(this.child[0].el);
+            this.tbody.appendChild(this.child[0].el);
         }
+    };
+    ItemGroup.prototype.renderGroup = function(table_map) {
+        for(var i = 0; i < table_map.length; i++) {
+            this.parseCell(table_map[i], i);
+        }
+        this.syncSelect('item_position_select');
+        this.syncCheckbox('group_item_checkbox');
+        this.syncCheckbox('sell_checkbox');
+        this.child.reverse();
     };
 
     ItemGroup.prototype.shrink = function() {
@@ -344,39 +388,79 @@
             });
         }
     };
+    function indexOf(obj, value) {
+        var tmp = Object.entries(LANGUAGE);
+        var idx = tmp.findIndex(function(pair){
+            return pair[1] === value;
+        });
+        if(idx > 0) {
+            return tmp[idx][0];
+        } else {
+            return '';
+        }
+    }
 
     function init() {
-        function parseRow(row) {
-            var item = new Item(row);
-            if(!item_db[item.name]) {
-                item_db[item.name] = new ItemGroup();
+        var table = findTable();
+        if(!(table && table.tHead && table.tBodies[0].rows.length > 1)) { return false; }
+
+        function parseRow(item_db, table) {
+            var row = table.tBodies[0].rows[0];
+            var item = new Item(row, table_map);
+            if(!item_db[item[group_by]]) {
+                item_db[item[group_by]] = new ItemGroup(table, group_by);
             }
-            item_db[item.name].add(item);
+            item_db[item[group_by]].add(item);
         }
 
-        var index = 0;
-        var item_db = {};
-        var table = findTable();
-        addCss();
-        if(table && table.tBodies[0].rows.length > 1) {
-            while(table.tBodies[0].rows[0]) {
-                parseRow(table.rows[2]);
-                table.rows[2].remove();
+        function parseTableIndex() {
+            var arr =  [];
+            function parseCell(head_cell) {
+                var btn, title = (btn = head_cell.querySelector('input[type="submit"]')) ? btn.value : head_cell.textContent;
+                title = chomp(title);
+                return indexOf(LANGUAGE, title).toLowerCase();
             }
+            for(var i = 0; i < head.children.length; i++) {
+                arr.push(parseCell(head.children[i]));
+            }
+            arr[0] = 'index';
+            return arr;
+        }
 
+        function parseTable(table) {
+            var item_db = {};
+            var tbody = table.tBodies[0];
+            while(tbody.rows[0]) {
+                parseRow(item_db, table);
+                tbody.rows[0].remove();
+            }
+            return item_db;
+        }
+
+        function initItemGroup(item_db) {
+            var index = Number(item_db[Object.keys(item_db)[0]].child[0].el.cells[0].textContent);
             for(var k in item_db) {
                 if(item_db.hasOwnProperty(k)){
-                    item_db[k].child.forEach((c) => (c.el.cells[0].textContent = ++index));
-                    item_db[k].render(table);
+                    item_db[k].child.forEach((c) => (c.el.cells[0].textContent = index++));
+                    item_db[k].render(table_map);
                 }
             }
-            for(k in item_db) { // Need 2 loop, or the table color would go wrong.
+            for(k in item_db) {
                 if(item_db.hasOwnProperty(k)){
                     item_db[k].toggleChild();
                 }
             }
         }
-    }
 
+
+        var head = table.tHead.rows[1];
+        var table_map = parseTableIndex(head);
+        var group_by = indexOf(LANGUAGE, chomp(head.querySelector('.table_hl_sorted').value)).toLowerCase();
+
+        if(/item|owner|storage_date|drop_date/.test(group_by)) {
+            addCss();
+            initItemGroup(parseTable(table));
+        }
+    }
     init();
 })();
